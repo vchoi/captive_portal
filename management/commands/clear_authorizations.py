843b1cee4a5046f82e1df35f5576ee3dfec1b578
= <vchoi@vchoi.org>
2014-02-28 17:56:11 -0300

    admin command clear_authorizations now resets firewall rules

--- management/commands/clear_authorizations.py
+++ management/commands/clear_authorizations.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from captive_portal.models import DeviceAuthorization
+from captive_portal.util import Firewall
 
 class Command(BaseCommand):
 	args = ''
@@ -7,3 +8,6 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		DeviceAuthorization.objects.all().delete()
+		fw = Firewall()
+		fw.reload_rules()
+		
